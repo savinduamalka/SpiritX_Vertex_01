37ea3120bf3e3dba2e0e199f2ce2137998acb0c8
AnjaleeAmarakoon <amarakoonanjalee11@gmail.com>
2025-03-08 14:11:25 +0530

    Created Login page

--- Auth Front-end/src/main.tsx
+++ Auth Front-end/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import Login from './Login.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+      
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<App />} /> {/* Assuming App is currently your signup page */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
