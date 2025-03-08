e624ce05aabde03acc22635835e58fd1099838e9
savinduamalka <savinduamalka@gmail.com>
2025-03-08 09:07:40 +0530

    connect with database

--- Auth Back-end/index.js
+++ Auth Back-end/index.js
@@ -1,20 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(express.json());
 
-// Sample Route
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Failed to connect to MongoDB", err));
+
 app.get("/", (req, res) => {
     res.send("Hello, Express!");
 });
 
-// Start Server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
