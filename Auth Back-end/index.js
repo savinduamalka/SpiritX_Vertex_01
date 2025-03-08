8c3f983cb39c2c979d31036ff949f11a7c7960d0
savinduamalka <savinduamalka@gmail.com>
2025-03-08 10:16:14 +0530

    remove confirm password index

--- Auth Back-end/index.js
+++ Auth Back-end/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import jwt from 'jsonwebtoken';
 import userRouter from "./routes/userRoutes.js";
+import User from "./models/userModel.js";
 
 dotenv.config();
 
@@ -33,7 +34,21 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log("Connected to MongoDB"))
+.then(async () => {
+    console.log("Connected to MongoDB");
+
+    // Remove the confirmPassword index if it exists
+    try {
+        await User.collection.dropIndex("confirmPassword_1");
+        console.log("confirmPassword index removed");
+    } catch (err) {
+        if (err.code === 27) {
+            console.log("confirmPassword index does not exist");
+        } else {
+            console.error("Error removing confirmPassword index", err);
+        }
+    }
+})
 .catch((err) => console.error("Failed to connect to MongoDB", err));
 
 app.get("/", (req, res) => {
